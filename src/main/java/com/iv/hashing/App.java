9176a0ee5fd348d524790c4298461415a66a8904
premprrohan <premprrohan@gmail.com>
2019-02-25 19:53:19 +0530

    java 8 compatiblity added

--- src/main/java/com/iv/hashing/App.java
+++ src/main/java/com/iv/hashing/App.java
@@ -1,5 +1,7 @@
 package com.iv.hashing;
 
+import org.apache.commons.codec.digest.DigestUtils;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        if (args.length <1){
+            System.err.println("Please provide an input!");
+            System.exit(0);
+        }
+        System.out.println(sha256hex(args[0]));
+
+    }
+
+    public static String sha256hex(String input) {
+        return DigestUtils.sha256Hex(input);
     }
 }
